8c538cf8c7a61bd83879ec2d8dbea1eccd9b8283
mailsujatha1@gmail.com <mailsujatha1@gmail.com>
2024-12-16 13:43:56 -0600

    Class Based Example

--- React/tutorial2/tgnews/src/App.js
+++ React/tutorial2/tgnews/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassBasedcomponent from './basics/class-based-component';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         TGNews
+        <ClassBasedcomponent></ClassBasedcomponent>
       </header>
     </div>
   );
---
+++ React/tutorial2/tgnews/src/basics/class-based-component.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class ClassBasedcomponent extends Component{
+
+    render(){
+
+        return (
+
+            <div>
+                <h1> Class Based Component</h1>
+            </div>
+        )
+
+
+    }
+}
+
+export default ClassBasedcomponent;

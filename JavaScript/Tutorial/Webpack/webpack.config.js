682c32069d455809f31585b5ab97343b2e024810
mailsujatha1@gmail.com <mailsujatha1@gmail.com>
2024-12-25 14:38:45 -0600

    webpack2

--- JavaScript/Tutorial/Webpack/webpack.config.js
+++ JavaScript/Tutorial/Webpack/webpack.config.js
@@ -5,6 +5,10 @@ module.exports = {
     entry: {
         bundle: path.resolve(__dirname, 'src/index.js')
     },
-    put 
+    output:{
+
+        path:path.resolve(__dirname, 'dist'),
+        filename:'[name].js',
+    },
 
 }

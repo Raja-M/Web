359a977f2c6828299adfecd0e1192b4a1ad2318a
Raja M <srinivasmrv@gmail.com>
2024-12-27 11:33:01 -0600

    nx workspace

--- JavaScript/Tutorial/Webpack/webpack.config.js
+++ JavaScript/Tutorial/Webpack/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWepackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     mode: 'development',
@@ -8,7 +10,59 @@ module.exports = {
     output:{
 
         path:path.resolve(__dirname, 'dist'),
-        filename:'[name].js',
+        filename:'[name][contenthash].js',
+        clean: true,
+        assetModuleFilename: '[name][ext]'
+
+  
     },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve( __dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+
+            },
+            /*
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            */
+            {
+                test: /\.(png|svg|jpg|jpeg)$/i,
+                type: 'asset/resource'
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWepackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/template.html'
+        }),
+        new BundleAnalyzerPlugin(),
+    ],
 
 }
